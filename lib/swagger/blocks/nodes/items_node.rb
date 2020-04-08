f433cedc3a9f70a7a26cc859145a7e043f4677ef
Rüdiger Franke <mail@ruedigerfranke.net>
2020-04-08 15:33:45 +0200

    one_of to items_node

--- lib/swagger/blocks/nodes/items_node.rb
+++ lib/swagger/blocks/nodes/items_node.rb
@@ -12,6 +12,11 @@ module Swagger
         def items(inline_keys = nil, &block)
           self.data[:items] = Swagger::Blocks::Nodes::ItemsNode.call(version: version, inline_keys: inline_keys, &block)
         end
+
+        def one_of(&block)
+          self.data[:oneOf] ||= []
+          self.data[:oneOf] << Swagger::Blocks::Nodes::OneOfNode.call(version: version, &block)
+        end
       end
     end
   end
